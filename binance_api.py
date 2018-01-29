c54ea4fe8acf3b951ac20c23a2993dd8f084a591
dkorolev <korwork87@yahoo.com>
2018-01-29 19:25:58 +0300

    final methods

--- binance_api.py
+++ binance_api.py
@@ -3,20 +3,24 @@ import http.client
 import json
 import logging
 import urllib.parse
-from collections import OrderedDict
-
-import requests
 import hmac
-
 import time
 
 from exceptions import ApiError
 
+# Class uses API method from Binance.
+# information about API
+# https://github.com/binance-exchange/binance-official-api-docs/blob/master/rest-api.md
+# https://support.binance.com/hc/en-us/articles/115003235691-Binance-API-Trading-Rules
+
 API_URL = 'api.binance.com'
-API_VERSION = 'v3'
+API_VERSION_3 = 'v3'
+API_VERSION_1 = 'v1'
 
 API_KEY = 'Gowa5xGG3PdCWa5ciCrZeTPXGqrUNy7Qh4MKHXiKUGMvaLM4GAhgIGJlbaFIZVlV'
 API_SECRET = 'avIfwd471KlubRpXNHyV2lH9cgRZLfgGngNvBCx2SyzEDVlTbhGqF2vQqiATGUfc'
+
+TRADES_LIMIT = '100'
 
 logger = logging.getLogger('xmb')
 
@@ -25,8 +29,8 @@ class BinanceApi:
 
     def get_open_orders(self, currency_1, currency_2):
         try:
-            timestamp = int(time.time() * 1000)
-            url = "/api/" + API_VERSION + "/" + 'openOrders' \
+            timestamp = BinanceApi._calculateTimestamp()
+            url = "/api/" + API_VERSION_3 + "/" + 'openOrders' \
                   + '?symbol='+currency_1+currency_2 + '&timestamp='+str(timestamp)
             return BinanceApi._call_binance_api(url,'GET', symbol=currency_1+currency_2, timestamp=timestamp)
         except KeyError:
@@ -35,8 +39,8 @@ class BinanceApi:
 
     def get_canceled_orders(self, currency_1, currency_2):
         try:
-            timestamp = int(round(time.time() * 1000))
-            url = "/api/" + API_VERSION + "/" + 'allOrders' \
+            timestamp = BinanceApi._calculateTimestamp()
+            url = "/api/" + API_VERSION_3 + "/" + 'allOrders' \
                   + '?symbol=' + currency_1 + currency_2 + '&timestamp=' + str(timestamp)
             obj = BinanceApi._call_binance_api(url,'GET',symbol=currency_1+currency_2, timestamp=timestamp)
             print(obj)
@@ -51,8 +55,8 @@ class BinanceApi:
 
     def is_order_partially_completed(self, currency_1, currency_2, order_id):
         try:
-            timestamp = int(round(time.time() * 1000))
-            url = "/api/" + API_VERSION + "/" + 'order' \
+            timestamp = BinanceApi._calculateTimestamp()
+            url = "/api/" + API_VERSION_3 + "/" + 'order' \
                   + '?orderId=' + str(order_id) + '&symbol=' + currency_1 + currency_2 + '&timestamp=' + str(timestamp)
             BinanceApi._call_binance_api(url, 'GET', symbol=currency_1 + currency_2, orderId=order_id,
                                                   timestamp=timestamp)
@@ -65,21 +69,21 @@ class BinanceApi:
 
     def cancel_order(self, currency_1, currency_2, order_id):
         logger.info('Cancel order %s', order_id)
-        timestamp = int(round(time.time() * 1000))
-        url = "/api/" + API_VERSION + "/" + 'order'
+        timestamp = BinanceApi._calculateTimestamp()
+        url = "/api/" + API_VERSION_3 + "/" + 'order'
         return BinanceApi._call_binance_api(url, 'DELETE', symbol=currency_1 + currency_2, orderId=order_id,
                                      timestamp=timestamp)
 
     def get_balances(self):
-        timestamp = int(round(time.time() * 1000))
-        url = "/api/" + API_VERSION + "/" + 'account'+ '?timestamp=' + str(timestamp)
+        timestamp = BinanceApi._calculateTimestamp()
+        url = "/api/" + API_VERSION_3 + "/" + 'account' + '?timestamp=' + str(timestamp)
         return BinanceApi._call_binance_api(url, 'GET', timestamp=timestamp)['balances']
 
 
     def create_order(self, currency_1, currency_2, quantity, price, side):
         logger.info('Create %s order (quantity=%s, price=%s)', side, quantity, price)
-        timestamp = int(round(time.time() * 1000))
-        url = "/api/" + API_VERSION + "/" + 'order/test'
+        timestamp = BinanceApi._calculateTimestamp()
+        url = "/api/" + API_VERSION_3 + "/" + 'order/test'
         return BinanceApi._call_binance_api(
             url, http_method='POST',
             symbol=currency_1 + currency_2,
@@ -91,37 +95,40 @@ class BinanceApi:
             timeInForce='GTC'
         )['order_id']
 
-    # def prices(self):
-    #     BinanceApi._call_binance_api("ticker/allPrices")
+
+    def get_trades(self, currency_1, currency_2):
+        url = "/api/" + API_VERSION_1 + "/" + 'trades' + '?symbol=' + currency_1 + currency_2 + '&limit=' + TRADES_LIMIT
+        return BinanceApi._call_binance_api(url, 'GET')
+
+    def get_user_trades(self, currency_1, currency_2, offset=0, limit=500):
+        timestamp = BinanceApi._calculateTimestamp()
+        url = "/api/" + API_VERSION_3 + "/" + 'myTrades' + '?limit=' + str(limit) + '&symbol=' + currency_1 + currency_2 \
+              + '&timestamp=' + str(timestamp)
+        return BinanceApi._call_binance_api(url, 'GET', limit = limit, symbol=currency_1 + currency_2, timestamp=timestamp)
 
 
     @staticmethod
     def _call_binance_api(url, http_method, **kwargs):
         logger.debug('Call Binance api. Url {}. Payload: {}'.format(url, kwargs))
 
+        payload = []
         if kwargs:
              payload = sorted(kwargs.items())
         payload = urllib.parse.urlencode(payload)
 
-        # H = hmac.new(key=API_SECRET.encode('utf-8'), digestmod=hashlib.sha256)
-        # H.update(payload.encode('utf-8'))
-        # sign = H.hexdigest()
         sign = hmac.new(API_SECRET.encode('utf-8'), payload.encode('utf-8'), digestmod=hashlib.sha256).hexdigest()
 
-        payload += '&signature=' + sign
-
-        # url = "/api/" + API_VERSION + "/" + api_method
-        # if http_method == 'GET':
-        #      url += '?symbol='+symbol + '&timestamp='+str(timestamp)  +'&signature='+sign
         if http_method == 'GET':
-            url+= '&signature='+sign
+            if API_VERSION_1 not in url:
+                url+= '&signature='+sign
+        else:
+            payload += '&signature=' + sign
 
         headers = {"Content-type": "application/x-www-form-urlencoded",
                    "X-MBX-APIKEY": API_KEY,
                    "signature": sign}
         conn = http.client.HTTPSConnection(API_URL, timeout=60)
         conn.request(http_method, url, payload, headers)
-        # conn.request(http_method, "/api/" + API_VERSION + "/" + api_method)
         response = conn.getresponse().read()
         conn.close()
         try:
@@ -129,9 +136,13 @@ class BinanceApi:
             logger.debug('Received response: {}'.format(response))
             if 'error' in obj and obj['error']:
                 raise ApiError(obj['error'])
-            # if 'code' in obj and obj['code']:
-            #     raise ApiError(obj['code'],obj['msg'])
-            print(obj['orderId'])
+            if 'code' in obj and obj['code']:
+                raise ApiError(obj['code'],obj['msg'])
+            print(obj)
             return obj
         except json.decoder.JSONDecodeError:
             raise ApiError('Ошибка анализа возвращаемых данных, получена строка', response)
+
+    @staticmethod
+    def _calculateTimestamp():
+        return int(round(time.time() * 1000))
--- test/test_binance_api.py
+++ test/test_binance_api.py
@@ -23,9 +23,11 @@ if __name__ == '__main__':
     # for x in range(1,10):
     # api.get_canceled_orders('LTC','BTC')
     # api.is_order_partially_completed('LTC','BTC',1)
-    api.cancel_order('LTC','BTC',2)
+    # api.cancel_order('LTC','BTC',2)
     # api.get_balances()
     # api.create_order('BTC','USDT',0.01,12000,'BUY')
+    # api.get_trades('BTC','USDT')
+    api.get_user_trades('BTC','USDT',0,500)
 
 # def prices(self):
 #     """Get latest prices for all symbols."""
