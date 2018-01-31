cd2d634f14c57f7834e5218422b068e80b620e74
ozavorot <Olga.Zavorotkina@t-systems.com>
2018-01-31 12:55:40 +0300

    api keys -> command line

--- exmo_api.py
+++ exmo_api.py
@@ -9,9 +9,6 @@ import urllib.parse
 
 from exceptions import ApiError
 
-API_KEY = 'K-0adad660e6aca39f90d3544d3ca07d566ca8054a'
-API_SECRET = b'S-fb8de5372bb6543fa609283b49c5b398fa4c2e59'
-
 API_URL = 'api.exmo.me'
 API_VERSION = 'v1'
 
@@ -19,22 +16,26 @@ logger = logging.getLogger('xmb')
 
 class ExmoApi:
 
+    def __init__(self, api_key, api_secret):
+        self._api_key = api_key
+        self._api_secret = api_secret.encode('ascii')
+
     def get_open_orders(self, currency_1, currency_2):
         try:
-            return ExmoApi._call_api('user_open_orders')[currency_1 + '_' + currency_2]
+            return self._call_api('user_open_orders')[currency_1 + '_' + currency_2]
         except KeyError:
             logger.debug('No open market orders')
             return []
 
     def get_canceled_orders(self, currency_1, currency_2):
         try:
-            return ExmoApi._call_api('user_cancelled_orders')[currency_1 + '_' + currency_2]
+            return self._call_api('user_cancelled_orders')[currency_1 + '_' + currency_2]
         except KeyError:
             return []
 
     def is_order_partially_completed(self, order_id):
         try:
-            trades = ExmoApi._call_api('order_trades', order_id=order_id)
+            trades = self._call_api('order_trades', order_id=order_id)
         except Exception as e:
             if 'Error 50304' in str(e):
                 return False
@@ -44,14 +45,14 @@ class ExmoApi:
 
     def cancel_order(self, order_id):
         logger.info('Cancel order %s', order_id)
-        return ExmoApi._call_api('order_cancel', order_id=order_id)
+        return self._call_api('order_cancel', order_id=order_id)
 
     def get_balances(self):
-        return ExmoApi._call_api('user_info')['balances']
+        return self._call_api('user_info')['balances']
 
     def create_order(self, currency_1, currency_2, quantity, price, type):
         logger.info('Create %s order (quantity=%s, price=%s)', type, quantity, price)
-        return str(ExmoApi._call_api(
+        return str(self._call_api(
             'order_create',
             pair=currency_1 + '_' + currency_2,
             quantity=quantity,
@@ -60,14 +61,13 @@ class ExmoApi:
         )['order_id'])
 
     def get_trades(self, currency_1, currency_2):
-        return ExmoApi._call_api('trades', pair=currency_1 + '_' + currency_2)[currency_1 + '_' + currency_2]
+        return self._call_api('trades', pair=currency_1 + '_' + currency_2)[currency_1 + '_' + currency_2]
 
     def get_user_trades(self, currency_1, currency_2, offset=0, limit=100):
-        return ExmoApi._call_api('user_trades', pair=currency_1 + '_' + currency_2, offset=offset, limit=limit)[
+        return self._call_api('user_trades', pair=currency_1 + '_' + currency_2, offset=offset, limit=limit)[
             currency_1 + '_' + currency_2]
 
-    @staticmethod
-    def _call_api(api_method, http_method="POST", **kwargs):
+    def _call_api(self, api_method, http_method="POST", **kwargs):
         logger.debug('Call Exmo api. Method {}. Payload: {}'.format(api_method, kwargs))
         payload = {'nonce': int(round(time.time() * 1000))}
 
@@ -75,12 +75,12 @@ class ExmoApi:
             payload.update(kwargs)
         payload = urllib.parse.urlencode(payload)
 
-        H = hmac.new(key=API_SECRET, digestmod=hashlib.sha512)
+        H = hmac.new(key=self._api_secret, digestmod=hashlib.sha512)
         H.update(payload.encode('utf-8'))
         sign = H.hexdigest()
 
         headers = {"Content-type": "application/x-www-form-urlencoded",
-                   "Key": API_KEY,
+                   "Key": self._api_key,
                    "Sign": sign}
         conn = http.client.HTTPSConnection(API_URL, timeout=60)
         conn.request(http_method, "/" + API_VERSION + "/" + api_method, payload, headers)
